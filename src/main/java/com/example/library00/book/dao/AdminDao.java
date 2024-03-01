947f141ef8f189fe8e0b4c970008cd574d9240a5
lhd <2303848231@qq.com>
2024-03-01 23:33:16 +0800

    YEYlibrary

--- src/main/java/com/example/library00/book/dao/AdminDao.java
+++ src/main/java/com/example/library00/book/dao/AdminDao.java
@@ -4,7 +4,6 @@ import com.example.library00.book.domain.Admin;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -15,7 +14,7 @@ import java.util.Map;
 public class AdminDao implements AdminMapper{
     @Autowired
     SqlSessionTemplate sqlSessionTemplate;
-    private final static String NAMESPACE = "com.example.library00.book.dao.BookMapper.";
+    private final static String NAMESPACE = "com.example.library00.book.dao.AdminMapper.";
     public int getMatchCount(long adminId,String password){
         Map<String,Object> paramMap = new HashMap<>();
         paramMap.put("adminId",adminId);
@@ -26,18 +25,14 @@ public class AdminDao implements AdminMapper{
         Map<String,Object> paramMap = new HashMap<>();
         paramMap.put("adminId",adminId);
         paramMap.put("newPasswd",newPasswd);
-        return sqlSessionTemplate.update(NAMESPACE+"rePassword",paramMap);
-        //return jdbcTemplate.update(RE_PASSWORD_SQL,new Object[]{newPasswd,adminId});
-    }
+        return sqlSessionTemplate.update(NAMESPACE+"rePassword",paramMap);}
     public String getPasswd(long id){
         return sqlSessionTemplate.selectOne(NAMESPACE+"getPasswd",id);
-        //return jdbcTemplate.queryForObject(GET_PASSWD_SQL,new Object[]{id},String.class);
     }
     public boolean findAdminId(long id) {
         int count = sqlSessionTemplate.selectOne(NAMESPACE + "findAdminId", id);
         return count > 0;
     }
-//        return jdbcTemplate.queryForObject(FIND_ADMIN_ID,new Object[]{id},Long.class) > 0;
 
     public void addAdmin(long adminId,String password){
         Admin admin= new Admin();
